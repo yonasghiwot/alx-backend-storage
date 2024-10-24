58aa2f0476244ed2ae437638684cc03c24b3306f
Yonas Gebrehiwot <yonihabeshaw@gmail.com>
2024-10-24 19:56:05 +0300

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Module for implementing an expiring web Caches and tracker
+"""
+import requests
+import time
+from functools import wraps
+
+CACHE_EXPIRATION_TIME = 10  
+CACHE = {}
+
+
+def cache(fn):
+    """_summary_
+
+    Args:
+        fn (function): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        url = args[0]
+        if url in CACHE and CACHE[url]["timestamp"] + CACHE_EXPIRATION_TIME > \
+                time.time():
+            CACHE[url]["count"] += 1
+            return CACHE[url]["content"]
+        else:
+            content = fn(*args, **kwargs)
+            CACHE[url] = {"content": content,
+                          "timestamp": time.time(), "count": 1}
+            return content
+    return wrapped
+
+
+@cache
+def get_page(url: str) -> str:
+    """_summary_
+
+    Args:
+        url (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    global count
+    count += 1
+    response = requests.get(url)
+    return response.content.decode('utf-8')
